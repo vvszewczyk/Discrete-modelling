7067f23ed72ab2fad83edf51292a2d766aa0f604
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2024-12-30 16:42:44 +0100

    Add definition

--- LBM/LBM/include/kernels.h
+++ LBM/LBM/include/kernels.h
@@ -5,6 +5,10 @@
 void collision(Cell* d_grid, int width, int height, double tau);
 void streaming(Cell* d_gridInput, Cell* d_gridOutput, int width, int height);
 
+// i:    0, 1, 2, 3, 4, 5, 6, 7, 8
+// opp:  0, 2, 1, 4, 3, 7, 8, 5, 6
+int findOpposite(int i); // Find opposite direction based on i 
+
 // Wrapper functions to call from host code
 void collisionWrapper(Cell* d_grid, int width, int height, double tau);
 void streamingWrapper(Cell* d_gridInput, Cell* d_gridOutput, int width, int height);
