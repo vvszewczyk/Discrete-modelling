cb7df11ec13c56b541a323dfc219b01ef439b3f0
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2024-12-30 18:39:32 +0100

    Refactor code for fluid flow, change block size
    idk why it cannot be 32x32

--- LBM/LBM/src/kernels.cu
+++ LBM/LBM/src/kernels.cu
@@ -1,9 +1,27 @@
 #include <cuda_runtime.h>
 #include "device_launch_parameters.h"
 #include "Cell.h"
+#include "kernels.h"
 
 // Documentation: CUDA Programming Guide
 // https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html
+
+__device__ __host__ int findOpposite(int i)
+{
+    switch (i)
+    {
+    case 0: return 0;
+    case 1: return 2;
+    case 2: return 1;
+    case 3: return 4;
+    case 4: return 3;
+    case 5: return 7;
+    case 6: return 8;
+    case 7: return 5;
+    case 8: return 6;
+    }
+    return 0; // default
+}
 
 __global__ void collision(Cell* grid, int width, int height, double tau)
 {
@@ -25,32 +43,57 @@ __global__ void collision(Cell* grid, int width, int height, double tau)
         return;
     }
 
-    double C = 0.0;
-    // Calcualte concentration in cell (sum of distribution function - 9)
-    for (int i = 0; i < 4; ++i)
+    double rho = 0.0;
+    // Calcualte rho in cell (sum of distribution function)
+    for (int i = 0; i < Q9; ++i)
     {
-        C += cell.getF_in(i);
+        rho += cell.getF_in(i);
     }
 
-    cell.setC(C);
+    // ux * rho, uy * rho
+    double uxRho = 0.0;
+    double uyRho = 0.0;
 
-    // Weights D2Q4
-    double w = 1.0 / 4.0;
-
-    // Calculate f_eq (equilibrium distribution function - 10)
-    for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < Q9; ++i)
     {
-        double f_eq = w * C;
-        cell.setF_eq(i, f_eq);
+        uxRho += cell.getF_in(i) * cx[i];
+        uyRho += cell.getF_in(i) * cy[i];
     }
 
-    // Collisions: f_out = f_in + (1.0 / tau) * (f_eq - f_in) - 11
-    for (int i = 0; i < 4; ++i)
+    double ux = 0.0;
+    double uy = 0.0;
+
+    if (rho > 1e-14)
     {
-        double f_in = cell.getF_in(i);
-        double f_eq = cell.getF_eq(i);
-        double f_out = f_in + (1.0 / tau) * (f_eq - f_in);
-        cell.setF_out(i, f_out);
+        ux = uxRho / rho;
+        uy = uyRho / rho;
+    }
+
+    cell.setRho(rho);
+    cell.setUx(ux);
+    cell.setUy(uy);
+
+    // Calculate f_eq
+    // f_i^{eq} = w_i * rho * [1 + 3(c_i.u) + 4.5(c_i.u)^2 - 1.5 * (u^2)]
+    for (int i = 0; i < Q9; ++i)
+    {
+        double c_iu = cx[i] * ux + cy[i] * uy; // (c_i · u)
+        double uu = ux * ux + uy * uy; // u^2
+        double term1 = 1.0 + (3.0 * c_iu);
+        double term2 = 4.5 * (c_iu * c_iu);
+        double term3 = -1.5 * uu;
+        double feq = w[i] * rho * (term1 + term2 + term3);
+        cell.setF_eq(i, feq);
+    }
+
+
+    // Collisions: fout = fin + (1.0 / tau) * (feq - fin)
+    for (int i = 0; i < Q9; ++i)
+    {
+        double fin = cell.getF_in(i);
+        double feq = cell.getF_eq(i);
+        double fout = fin + (1.0 / tau) * (feq - fin);
+        cell.setF_out(i, fout);
     }
 }
 
@@ -68,15 +111,12 @@ __global__ void streaming(Cell* gridInput, Cell* gridOutput, int width, int heig
 
     Cell outCell;
     outCell.setWall(inCell.getWall());
-    outCell.setC(inCell.getC());
-
-    int cx[4] = { 0, 1, 0, -1 };
-    int cy[4] = { 1, 0, -1, 0 };
-
-    int oppositeDir[4] = { 2, 3, 0, 1 };
+    outCell.setRho(inCell.getRho());
+    outCell.setUx(inCell.getUx());
+    outCell.setUy(inCell.getUy());
 
     // For each f_in in outCell, we look for where f_out comes from:
-    for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < Q9; ++i)
     {
         // Coordinates of neighbour cell
         int nx = x - cx[i];
@@ -85,27 +125,26 @@ __global__ void streaming(Cell* gridInput, Cell* gridOutput, int width, int heig
         double f_inVal = 0.0;
 
         // Check if neighbor cell is within bounds
-        if (nx >= 0 && nx < width && ny >= 0 && ny < height)
+        if (nx < 0 || nx >= width || ny < 0 || ny >= height)
+        {
+            int opposite = findOpposite(i);
+            f_inVal = inCell.getF_out(opposite);
+        }
+        else
         {
             int neighborIdx = ny * width + nx;
             const Cell& neighborCell = gridInput[neighborIdx];
 
             if (neighborCell.getWall())
             {
-                // bounce back
-                // 0 <-> 2 (N <-> S)
-                // 1 <-> 3 (E <-> W)
-                f_inVal = neighborCell.getF_out(oppositeDir[i]);
+                // bounce back from wall
+                int opposite = findOpposite(i);
+                f_inVal = neighborCell.getF_out(opposite);
             }
             else // Just streaming
             {
                 f_inVal = neighborCell.getF_out(i);
             }
-        }
-        else
-        {
-            // Neighbor is out of bounds -> bounce back
-            f_inVal = inCell.getF_out(oppositeDir[i]);
         }
 
         outCell.setF_in(i, f_inVal);
@@ -118,7 +157,7 @@ __global__ void streaming(Cell* gridInput, Cell* gridOutput, int width, int heig
 
 void collisionWrapper(Cell* grid, int width, int height, double tau)
 {
-    dim3 blockSize(32, 32); // maxThreadsPerBlock = 1024
+    dim3 blockSize(16, 16); // maxThreadsPerBlock = 1024
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y); // Number of blocks required to cover all grid
 
     collision <<<gridSize, blockSize >>> (grid, width, height, tau); // Start kernel
@@ -127,7 +166,7 @@ void collisionWrapper(Cell* grid, int width, int height, double tau)
 
 void streamingWrapper(Cell* gridInput, Cell* gridOutput, int width, int height)
 {
-    dim3 blockSize(32, 32); // maxThreadsPerBlock = 1024
+    dim3 blockSize(16, 16); // maxThreadsPerBlock = 1024
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y); // Number of blocks required to cover all grid
 
     streaming <<<gridSize, blockSize >>> (gridInput, gridOutput, width, height); // Start kernel
