657a252ad08d7497643728149bbbfef7cb748ea6
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-01-21 19:17:29 +0100

    Update Particle.h

--- LBM/LBM/include/Particle.h
+++ LBM/LBM/include/Particle.h
@@ -3,34 +3,39 @@
 class Particle
 {
   public:
-    // Po³o¿enie w uk³adzie siatki (np. float, bo i tak bêdziemy interpolowaæ/zaokr¹glaæ)
+    // Coordinates
     float x;
     float y;
 
-    // Prêdkoœæ w³asna cz¹stki
+    // Velocity
     float vx;
     float vy;
 
-    // Parametry fizyczne
-    float mass; // "m" z instrukcji (zakres 0..1)
-    float g;    // grawitacja (np. 0.005f)
+    float m;    // mass
+    float grav; // gravity
 
-    // Kolor do rysowania
+    // Color
     float r;
-    float gcol; // "gcol" zamiast "g", by unikn¹æ kolizji nazwy
+    float g;
     float b;
 
-    // Flaga aktywnoœci (by np. wy³¹czyæ rysowanie, gdy wypadnie poza siatkê)
-    bool active;
+    bool active; // Activate/disactivate drawing
+    std::vector<std::pair<float, float>> trace;
 
-    // Konstruktor domyœlny
-    Particle() : x(0.f), y(0.f), vx(0.f), vy(0.f), mass(0.f), g(0.f), r(1.f), gcol(1.f), b(1.f), active(true)
+    // Path history
+    void addTrace()
+    {
+        trace.emplace_back(x, y);
+        if (trace.size() > 500000)
+            trace.erase(trace.begin());
+    }
+
+    Particle() : x(0.f), y(0.f), vx(0.f), vy(0.f), m(0.f), grav(0.f), r(1.f), g(1.f), b(1.f), active(true)
     {
     }
 
-    // Konstruktor z parametrami (opcjonalnie)
     Particle(float px, float py, float pmass, float pg, float pr, float pgcol, float pb)
-        : x(px), y(py), vx(0.f), vy(0.f), mass(pmass), g(pg), r(pr), gcol(pgcol), b(pb), active(true)
+        : x(px), y(py), vx(0.f), vy(0.f), m(pmass), grav(pg), r(pr), g(pgcol), b(pb), active(true)
     {
     }
 };
