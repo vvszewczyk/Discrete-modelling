2f864cf3935ca7cad8103149fe658830a3ed4667
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-01-23 10:42:30 +0100

    Add Partcle functions definitions save/load state definitions and labels

--- LBM/LBM/include/SimulationController.h
+++ LBM/LBM/include/SimulationController.h
@@ -2,8 +2,12 @@
 
 #include "CudaHandler.h"
 #include "Grid.h"
+#include "Particle.h"
 #include <GL/freeglut.h>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 class SimulationController
 {
@@ -14,24 +18,25 @@ class SimulationController
     int mainWindowID;
     int uxWindowID;
     int uyWindowID;
-
-    // Size of window
     int windowWidth;
     int windowHeight;
-    int cellSize; // Size of single cell in pixels
-
-    std::string buttonLabelSS; // Button label for stop/start
+    int cellSize;                    // Size of single cell in pixels
+    std::string buttonLabelSS;       // Button label for stop/start
+    std::string buttonLabelVariants; // Button label for variants
     bool placingWalls = false;
+    bool variant = false;
     bool isLeftMouseDown = false;
+    bool stateLoaded = false;
     std::string buttonLabelWE; // Button label for wall/empty
     int stepsPerFrame;         // For managing simulation speed
-
     bool gridModified;
     static const int minStepsPerFrame = 1;
     static const int maxStepsPerFrame = 30;
     int totalIterations;
-
-    double tau; // Relaxation time
+    double tau;                      // Relaxation time
+    std::vector<Particle> particles; // Placeholder for particles
+    void updateParticles();          // Particles movement logic
+    void initializeParticles(int numParticles);
 
   public:
     SimulationController(Grid *g, CudaHandler *ch, int width, int height, int cs);
@@ -40,8 +45,10 @@ class SimulationController
     // Manage simulation methods
     void startSimulation();
     void stopSimulation();
+    void resumeSimulation();
     void toggleSimulation();
     void toggleWallPlacement();
+    void toggleVariant();
     void resetSimulation();
 
     void initializeUI(int argc, char **argv);              // Initialise UI, set OpenGL and GLUT
@@ -58,4 +65,10 @@ class SimulationController
     static void staticMotionHandler(int x, int y);                 // Motion handler but static
 
     void updateSimulation(); // Update simulation state
+    void saveCurrentFrameBMP(const char *filename, int width, int height);
+    void saveSimulationStateCSV(const char *filename, const Grid *grid);
+    void loadSimulationStateCSV(const char *filename, Grid *grid);
+    void copyWindowPixelsToBigBuffer(unsigned char *srcPixels, int srcWidth, int srcHeight, unsigned char *dstPixels,
+                                     int dstTotalWidth, int dstTotalHeight,
+                                     int offsetX); // For saving 3 windows in one BMP
 };
