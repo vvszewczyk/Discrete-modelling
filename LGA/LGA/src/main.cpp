fac19ee3ef4df48f03c7f3aeb144f87e3ac1055e
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-02-11 14:04:20 +0100

    Add LGA/LBM gifs

--- LGA/LGA/src/main.cpp
+++ LGA/LGA/src/main.cpp
@@ -1,37 +1,36 @@
-#include "Grid.h"
 #include "CudaHandler.h"
+#include "Grid.h"
 #include "SimulationController.h"
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	// Grid size
-	const int gridWidth = 800;
-	const int gridHeight = 800;
+    // Grid size
+    const int gridWidth = 400;
+    const int gridHeight = 400;
 
-	cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, 0); // 0 is the device ID
-	std::cout << "Max threads per block: " << prop.maxThreadsPerBlock << std::endl;
-	std::cout << "Max threads per dimension: " << prop.maxThreadsDim[0] << ", " << prop.maxThreadsDim[1] << std::endl;
-	std::cout << "Shared memory per block: " << prop.sharedMemPerBlock << " bytes" << std::endl;
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0); // 0 is the device ID
+    std::cout << "Max threads per block: " << prop.maxThreadsPerBlock << std::endl;
+    std::cout << "Max threads per dimension: " << prop.maxThreadsDim[0] << ", " << prop.maxThreadsDim[1] << std::endl;
+    std::cout << "Shared memory per block: " << prop.sharedMemPerBlock << " bytes" << std::endl;
 
+    // Window size
+    const int windowWidht = 800;
+    const int windowHeight = 800;
+    const int cellSize = windowWidht / gridWidth;
 
-	// Window size
-	const int windowWidht = 800;
-	const int windowHeight = 800;
-	const int cellSize = windowWidht / gridWidth;
+    // Grid initializing
+    Grid grid(gridWidth, gridHeight);
+    grid.initialize();
 
-	// Grid initializing
-	Grid grid(gridWidth, gridHeight);
-	grid.initialize();
+    // CUDA initializing
+    CudaHandler cudaHandler(gridWidth, gridHeight);
+    cudaHandler.initializeDeviceGrids(grid.getGridData());
 
-	// CUDA initializing
-	CudaHandler cudaHandler(gridWidth, gridHeight);
-	cudaHandler.initializeDeviceGrids(grid.getGridData());
+    // SimulationController initializing
+    SimulationController controller(&grid, &cudaHandler, windowWidht, windowHeight, cellSize);
+    controller.initializeUI(argc, argv);
 
-	// SimulationController initializing
-	SimulationController controller(&grid, &cudaHandler, windowWidht, windowHeight, cellSize);
-	controller.initializeUI(argc, argv);
-
-	glutMainLoop();
+    glutMainLoop();
 }
